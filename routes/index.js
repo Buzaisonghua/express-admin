1f96544e30345154e2dd5dffa34b724d986b8712
wangbo <99548050@qq.com>
2025-01-09 12:43:39 +0800

    feature 配置环境变量

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/getUserInfo", function (req, res, next) {
+  res.json({ code: "2001" });
+});
+
+router.get("/dev-api/getUserInfo", function (req, res, next) {
+  res.json({ code: "201" });
+});
+router.get("/api/getUserInfo", function (req, res, next) {
+  res.json({ code: "20221" });
 });
 
 module.exports = router;
